67378188a796652d9624c9f0406c79f273342655
Lukas Wogrin <Lukas.Wogrin@gmx.at>
2026-05-04 12:28:27 +0200

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe30
+{
+    internal class Player
+    {
+        private string _name;
+        private int _healt;
+        private int _atk;
+        private decimal _money;
+
+        public string Name
+        { get { return _name; } }
+        public int Healt
+        { get { return _healt; } }
+        public int Attak
+        { get { return _atk; } }
+        public decimal Money
+        { get { return _money; } }
+
+        public Player(string name, int healt, int atk, decimal money)
+        {
+            _name = name;
+            _healt = healt;
+            _atk = atk;
+            _money = money;
+        }
+
+
+
+        public string ToString()
+        {
+            return $"Name: {_name}\r\nHealt: {_healt}\r\nAttack: {_atk}\r\nMoney: {_money}";
+        }
+
+        public void Geld(int einaus)
+        {
+            _money += einaus;
+        }
+    }
+        
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(File.Exists("Player.bin"))
+            {
+                ReadBinaryFile("Player.bin");
+            }
+            else
+            {
+                Player[] Arr = new Player[3]
+                {
+                new Player("Hearbert",100,10,10000),
+                new Player("Damian",120,15,(decimal)15.55),
+                new Player("Domaaenik",110, 12, (decimal)199.99)
+                };
+
+                WriteBinaryFile("Player.bin", Arr);
+                ReadBinaryFile("Player.bin");
+            }
+
+
+
+        }
+        private static void WriteBinaryFile(string path, Player[] Arr)
+        {
+            FileStream myStream = new FileStream(path, FileMode.Create);
+            BinaryWriter writer = new BinaryWriter(myStream);
+
+            writer.Write(Arr.Length);
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                writer.Write(Arr[i].Name);
+                writer.Write(Arr[i].Healt);
+                writer.Write(Arr[i].Attak);
+                writer.Write(Arr[i].Money);
+            }
+
+            writer.Close();
+            myStream.Close();
+        }
+        private static void ReadBinaryFile(string path)
+        {
+            FileStream myStream = new FileStream(path, FileMode.Open);
+            BinaryReader reader = new BinaryReader(myStream);
+
+            int anzahl = reader.ReadInt32();
+            Console.WriteLine(anzahl);
+
+            while (reader.PeekChar() > -1)
+            {
+                string name = reader.ReadString();
+                Console.WriteLine(name);
+
+                int healt = reader.ReadInt32();
+                Console.WriteLine(healt);
+
+                int atk = reader.ReadInt32();
+                Console.WriteLine(atk);
+
+                decimal money = reader.ReadDecimal();
+                Console.WriteLine(money);
+            }
+
+            reader.Close();
+            myStream.Close();
+        }
+    }
+}
